5109ea2a8ed311512dd44af1a9cde751bb8834f2
RhinoCodes <51250021+RhinoCodes@users.noreply.github.com>
2020-10-11 14:56:21 -0500

    Add C++ Version

---
+++ trajectory.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+
+int main(){
+	int speed=400;
+	int tilt=90;	
+	tilt = tilt - 90;
+	int xspeed=0;
+	if (tilt != 0) {
+		int xspeed = 400/(90/tilt);
+	} 
+
+	int yspeed = speed-xspeed;
+
+	int x = 0;
+	int y=100;
+
+	for(int i=0; i<10; i++) {
+		x = x + xspeed;
+		y = y + yspeed;
+		std::cout << "x: " << x << " y: " << y << std::endl;
+		usleep(500000);
+	}
+}
+
